004c094aad140d3afcc2825ea60038a8fc020be4
yihui <123>
2018-10-06 23:02:07 +0800

    添加模拟登录

---
+++ scrapydemo/spiders/demo.py
@@ -0,0 +1,4 @@
+from scrapy.http import Request, FormRequest, HtmlResponse
+
+# if __name__ == '__main__':
+#     FormRequest.from_response("")
--- scrapydemo/spiders/odianso.py
+++ scrapydemo/spiders/odianso.py
@@ -5,34 +5,36 @@ from scrapy.http import Request, FormRequest, HtmlResponse
 
 class OdiansoSpider(scrapy.Spider):
     name = 'odianso'
-    allowed_domains = ['z.dian.so']
+    allowed_domains = ['o.dian.so']
     #start_urls = ['https://o.dian.so/']
-    start_urls=['https://z.dian.so/shop/index/getShopList?t=1538818309418&data=%7B%22districtCode%22%3Anull%2C%22shopType%22%3Anull%2C%22status%22%3A-1%2C%22offset%22%3A0%2C%22pageSize%22%3A20%7D']
+    start_urls=['http://o.dian.so/leo/1.0/data/directDayStatistics/getShopDeviceInfo?t=1538837804339&data=%7B%22shopId%22%3A%22416946%22%7D']
 
     #login_url = ['http://o.dian.so/leo/1.0/h5/login?t=1538821359012&data={"mobile":"13285977597","token":"120501300104"}']
 
     def start_requests(self):
         start_url = 'http://o.dian.so/leo/1.0/h5/login?t=1538821359012&data={"mobile":"13285977597","token":"120501300104"}'
         return [
-            Request(start_url, callback=self.parseWelcome)
+            Request(start_url, callback=self.afterLogin)
         ]
 
 
-    def parseWelcome(self, response):
-        lt = response.xpath('//input[@name="lt"]/@value').extract_first()
-        #logging.info('lt:' + lt)
-        return FormRequest.from_response(
-            response,
-            url='http://o.dian.so/leo/1.0/h5/login?t=1538821359012&data={"mobile":"13285977597","token":"120501300104"}',
-            #meta={'cookiejar': response.meta['cookiejar']},
-            formdata={'data':'{"mobile":"13285977597","token":"120501300104"}', "lt" : lt},
-            callback=self.afterLogin
-        )
+    # def parseWelcome(self, response):
+    #
+    #     lt = response.xpath('//input[@name="lt"]/@value').extract_first()
+    #     #logging.info('lt:' + lt)
+    #     return FormRequest.from_response(
+    #         response,
+    #         url='http://o.dian.so/leo/1.0/h5/login?t=1538821359012&data={"mobile":"13285977597","token":"120501300104"}',
+    #         #meta={'cookiejar': response.meta['cookiejar']},
+    #         #formdata={'data':'{"mobile":"13285977597","token":"120501300104"}', "lt" : lt},
+    #         callback=self.afterLogin
+    #     )
 
     def afterLogin(self, response):
-        yield Request(self.start_urls)
+        #yield Request(self.start_urls)
+       yield Request('http://o.dian.so/leo/1.0/data/directDayStatistics/getShopDeviceInfo?t=1538837804339&data=%7B%22shopId%22%3A%22416946%22%7D',callback=self.parse)
 
     def parse(self, response):
-        print (response.text())
+        print (response.text)
 
         #pass
